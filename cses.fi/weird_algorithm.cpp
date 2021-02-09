acd608fb1efe545532134f31b9c883a8d9df0296
zhandosayupov <zhandosayupov@gmail.com>
2021-02-09 15:51:25 +0600

    zha

---
+++ cses.fi/weird_algorithm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+	ll n;cin>>n;
+	cout<<n<<" ";
+	while(n!=1){
+		if(!(n%2))n/=2;
+		else n=3*n+1;
+		cout<<n<<" ";
+	}
+}
